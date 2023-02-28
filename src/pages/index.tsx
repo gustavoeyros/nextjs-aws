ae29605c584c3b52fdf377342fbf53c31dae1d5c
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-02-28 15:00:57 -0300

    feat: fetch data from amplify api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,29 @@
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { useState, useEffect } from "react";
+import { API } from "aws-amplify";
+import { listPosts } from "../graphql/queries";
+import { ListPostsQuery } from "@/API";
 
 export default function Home() {
-  return <h1 className="text-6xl font-bold underline">Hello world!</h1>;
+  const [posts, setPosts] = useState<ListPostsQuery[] | any>([]);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    const postData = (await API.graphql({
+      query: listPosts,
+    })) as { data: ListPostsQuery; errors: Error[] };
+
+    setPosts(postData.data.listPosts?.items);
+  };
+
+  return (
+    <div>
+      <h1 className="text-6xl font-bold underline">My Posts!</h1>
+      {posts.map((posts: any, index: any) => (
+        <p key={index}>{posts.content}</p>
+      ))}
+    </div>
+  );
 }
