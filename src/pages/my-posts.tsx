0d48bbff7f7626a48c338060d3c24101e9387733
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-03-02 15:14:01 -0300

    feat: my posts page styled

--- src/pages/my-posts.tsx
+++ src/pages/my-posts.tsx
@@ -3,6 +3,8 @@ import { postsByUsername } from "@/graphql/queries";
 import { Auth, API } from "aws-amplify";
 import { useState, useEffect } from "react";
 import Link from "next/link";
+import Moment from "moment";
+import { deletePost } from "@/graphql/mutations";
 const MyPosts = () => {
   const [posts, setPosts] = useState<any>([]);
   useEffect(() => {
@@ -21,16 +23,33 @@ const MyPosts = () => {
 
   return (
     <div>
-      <h1 className="text-3xl font-semibold tracking-wide mt-6 mb-2">
-        My Posts
-      </h1>
       {posts.map((post: any, index: any) => (
-        <Link key={index} href={`/posts/${post.id}`}>
-          <div className="cursor-pointer border-b border-gray-300 mt-8 pb-4">
-            <h2 className="text-xl font-semibold">{post.title}</h2>
-            <p className="text-gray-500">Author: {post.username}</p>
+        <div
+          key={index}
+          className="py-8 px-8 max-w-xxl mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:items-center sm:space-y-0 sm:space-x-6 mb-2"
+        >
+          <div className="text-center space-y-2 sm:text-left">
+            <div className="space-y-0.5">
+              <p className="text-lg text-black font-semibold">{post.title}</p>
+              <p className="text-slate-500 font-medium">
+                Created on: {Moment(post.createdAt).format("ddd, MMM hh:mm a")}
+              </p>
+            </div>
+            <div className="sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-1">
+              <p className="px-4 py-1 text-sm text-purple-600 font-semibold border border-purple-600 rounded-3xl hover:text-white hover:bg-purple-600 hover:border-transparent focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+                <Link href={`/edit-post/${post.id}`}>Edit Post</Link>
+              </p>
+
+              <p className="px-4 py-1 text-sm text-purple-600 font-semibold border border-purple-600 rounded-3xl hover:text-white hover:bg-purple-600 hover:border-transparent focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+                <Link href={`/posts/${post.id}`}>View Post</Link>
+              </p>
+
+              <button className="text-sm mr-4 text-red-500 font-semibold">
+                Delete Post
+              </button>
+            </div>
           </div>
-        </Link>
+        </div>
       ))}
     </div>
   );
